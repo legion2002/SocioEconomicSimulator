e9ef64da628a952a2abd943f6ddecd1c04972242
Diksha Sethi <dikshasethi251@gmail.com>
2021-11-26 10:09:35 +0530

    Main file

---
+++ testingSimulation.py
@@ -0,0 +1,21 @@
+import simpy
+import random
+#import blobWorld.py
+
+def generateBlobs():
+	friendliness = randint(1, 10)
+	greediness = randint(1, 10)
+	productivity = randint(1,3)
+	motivation = randint(1, 10)
+	energy = 10
+	return 
+
+
+def main():
+	#Creating a list of blobs
+
+	blob_list = []
+
+	max_blobs = 100
+	num_simulations = 14
+
